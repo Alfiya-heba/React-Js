eb8a8c120b621f5b70f9721d8a4665a549df4d58
Alfiya Heba <alfiyaheba10@gmail.com>
2025-01-27 20:08:48 +0530

    basic react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Alfiya Heba | 27th Jan 2025</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+ 
+
+  return (
+   <h1>Alfiya Heba | Vite </h1>
+  )
+}
+
+export default App
