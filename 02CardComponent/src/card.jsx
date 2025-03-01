7e88f3dd097b5c2c30915ebf63a88d2e4209f585
Alfiya Heba <alfiyaheba10@gmail.com>
2025-03-01 12:46:19 +0530

    Card-Component

--- 02CardComponent/src/card.jsx
+++ 02CardComponent/src/card.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Card({image,name,description}){
     return(
+        <>
+       
         <div className="card">
             <img className="card-img" src={image} alt="{name}" />
             <h2 className="heading">{name}</h2>
             <p>{description}</p>
         </div>
+        </>
     );
 }
 
