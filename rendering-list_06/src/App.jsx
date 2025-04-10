c3bcc370b0e195d22876636f5ba0fe5f6b4cae48
Alfiya Heba <alfiyaheba10@gmail.com>
2025-04-10 21:11:02 +0530

    rendering lists

--- rendering-list_06/src/App.jsx
+++ rendering-list_06/src/App.jsx
@@ -1,12 +1,28 @@
 
 import List from './list.jsx'
 function App() {
+  const fruits=[{id:1,name:"Banana",quantity:3},
+                  {id:2,name:"Apple",quantity:1},
+                  {id:3,name:"Cherry",quantity:10},
+                  {id:4,name:"pineapple",quantity:50},
+                  {id:5,name:"strawberry",quantity:69},
+                  
+]
+
+const vegetables=[{id:6,name:"Potato",quantity:30},
+                  {id:7,name:"Tomato",quantity:12},
+                  {id:8,name:"Cucumber",quantity:15},
+                  {id:9,name:"Chilli",quantity:58},
+                  {id:10,name:"Pumpkin",quantity:66},
+                  
+]
  
 
   return (
-    <List/>
-    
-
+    <>
+    {fruits.length>0 ? <List items={fruits} category="Fruits" /> : null}
+    {vegetables.length> 0 ? <List items={vegetables} category="Vegetables"/> : null}
+    </>
   )
 }
 
--- rendering-list_06/src/list.jsx
+++ rendering-list_06/src/list.jsx
@@ -1,16 +1,24 @@
-function List(){
-    const fruits=[{id:1,name:"Banana",quantity:3},
-                  {id:2,name:"Apple",quantity:1},
-                  {id:3,name:"Cherry",quantity:10},
-                  
-]
-fruits.sort((a,b)=>a.name.localeCompare(b.name))
-fruits.sort((a,b)=>a.quantity-b.qunatity)
-    const lowCalfruits=fruits.filter(fruit=>fruit.quantity<9)
-    const fruitsItems=lowCalfruits.map(lowCalfruits=><li key={lowCalfruits.id}>
-                                {lowCalfruits.name}: &nbsp;
-                                <b>{lowCalfruits.quantity}</b></li>)
-    return(<ol>{fruitsItems}</ol>)
+function List(props){
+
+    const category=props.category;
+    const itemList=props.items;
+   
+// fruits.sort((a,b)=>a.name.localeCompare(b.name))
+// fruits.sort((a,b)=>a.quantity-b.qunatity)
+    // const lowCalfruits=fruits.filter(fruit=>fruit.quantity<9)
+    const fruitsItems=itemList.map(items=><li key={items.id}>
+                                {items.name}: &nbsp;
+                                <b>{items.quantity}</b></li>)
+    return(
+    <>
+    <h3 className="list-category">{category}</h3>
+    <ol className="list-items">{fruitsItems}</ol>
+   
+    </>);
 
 }
-export default List
+List.defaultProps={
+    category:"category"
+
+};
+export default List;
